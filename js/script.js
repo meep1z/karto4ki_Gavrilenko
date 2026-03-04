7c086e9762b902208bd48a5a9828245283d4f97b
meep1z <aristarhgavrilenko@gmail.com>
2026-03-04 11:10:31 +0200

    добавлена золотая карта и новая система очков

--- js/script.js
+++ js/script.js
@@ -1,20 +1,18 @@
 $(document).ready(() => {
-    $('#body').css('background-color', 'beige');
+  $('#body').css('background-color', 'beige');
 
-    $('.text').css
-    ({
-        color: 'black', 
-        fontSize: '32px'
-    });
+  $('.text').css({
+      color: 'black', 
+      fontSize: '32px'
+  });
 
-    $('.button').css
-    ({
-        'background': 'green',
-        'border-radius': '10px',
-        'cursor': 'pointer',
-        'fontSize': '28px',
-        'color': 'white'
-    });
+  $('.button').css({
+      'background': 'green',
+      'border-radius': '10px',
+      'cursor': 'pointer',
+      'fontSize': '28px',
+      'color': 'white'
+  });
 
     $('.grid').css
     ({
@@ -26,124 +24,192 @@ $(document).ready(() => {
     })
 
   const cardsarr = [
-    {
-      name: 'fries',
-      img: 'images/fries.png'
-    },
-    {
-      name: 'cheeseburger',
-      img: 'images/cheeseburger.png'
-    },
-    {
-      name: 'ice-cream',
-      img: 'images/ice-cream.png'
-    },
-    {
-      name: 'pizza',
-      img: 'images/pizza.png'
-    },
-    {
-      name: 'milkshake',
-      img: 'images/milkshake.png'
-    },
-    {
-      name: 'hotdog',
-      img: 'images/hotdog.png'
-    },
-    {
-      name: 'fries',
-      img: 'images/fries.png'
-    },
-    {
-      name: 'cheeseburger',
-      img: 'images/cheeseburger.png'
-    },
-    {
-      name: 'ice-cream',
-      img: 'images/ice-cream.png'
-    },
-    {
-      name: 'pizza',
-      img: 'images/pizza.png'
-    },
-    {
-      name: 'milkshake',
-      img: 'images/milkshake.png'
-    },
-    {
-      name: 'hotdog',
-      img: 'images/hotdog.png'
-    }
-  ]
+      {
+          name: 'fries',
+          img: 'images/fries.png',
+          isGold: false
+      },
+      {
+          name: 'cheeseburger',
+          img: 'images/cheeseburger.png',
+          isGold: false
+      },
+      {
+          name: 'ice-cream',
+          img: 'images/ice-cream.png',
+          isGold: false
+      },
+      {
+          name: 'pizza',
+          img: 'images/pizza.png',
+          isGold: false
+      },
+      {
+          name: 'milkshake',
+          img: 'images/milkshake.png',
+          isGold: false
+      },
+      {
+          name: 'hotdog',
+          img: 'images/hotdog.png',
+          isGold: false
+      },
+      {
+          name: 'fries',
+          img: 'images/fries.png',
+          isGold: false
+      },
+      {
+          name: 'cheeseburger',
+          img: 'images/cheeseburger.png',
+          isGold: false
+      },
+      {
+          name: 'ice-cream',
+          img: 'images/ice-cream.png',
+          isGold: false
+      },
+      {
+          name: 'pizza',
+          img: 'images/pizza.png',
+          isGold: false
+      },
+      {
+          name: 'milkshake',
+          img: 'images/milkshake.png',
+          isGold: false
+      },
+      {
+          name: 'hotdog',
+          img: 'images/hotdog.png',
+          isGold: false
+      }
+  ];
 
-  cardsarr.sort(() => 0.5 - Math.random())
+  const cards = ['fries', 'cheeseburger', 'ice-cream', 'pizza', 'milkshake', 'hotdog'];
+  const randomGoldCard = cards[Math.floor(Math.random() * cards.length)];
+  
+  cardsarr.forEach((card, index) => {
+      if (card.name === randomGoldCard) {
+          card.isGold = true;
+      }
+  });
 
-  const $grid = $('.grid')
-  const $result = $('#result')
-  const $current = $('#current')
-  let chosen = []
-  let chosenId = []
-  let won = []
-  let lock = false
+  cardsarr.sort(() => 0.5 - Math.random());
+
+  const $grid = $('.grid');
+  const $result = $('#result');
+  const $current = $('#current');
+  const $attempts = $('#attempts');
+  let chosen = [];
+  let chosenId = [];
+  let won = [];
+  let lock = false;
+  
+  let attempt = 0;
+  let founded = false;
+
+  let goldCardNum = 0;
+
 
   function create() {
-    for (let i = 0; i < cardsarr.length; i++) {
-      const $card = $('<img>')
-      $card.attr('src', 'images/blank.png')
-      $card.attr('data-id', i)
-      $card.on('click', flip)
-      $grid.append($card)
-    }
+      for (let i = 0; i < cardsarr.length; i++) {
+          const $card = $('<img>');
+          $card.attr('src', 'images/blank.png');
+          $card.attr('data-id', i);
+          
+          if (cardsarr[i].isGold) {
+              $card.addClass('gold-card');
+          }
+          
+          $card.on('click', flip);
+          $grid.append($card);
+      }
   }
 
-  $('#btn').click( () => {
-    location.reload()
-  })
+  $('#btn').click(() => {
+      location.reload();
+  });
+
+  function update() {
+    $attempts.text(attempt);
+}
 
   function check() {
-    const $cardsarr = $('img')
-    const oneId = chosenId[0]
-    const twoId = chosenId[1]
-    
-    if(oneId == twoId) {
-      $cardsarr.eq(oneId).attr('src', 'images/blank.png')
-      $cardsarr.eq(twoId).attr('src', 'images/blank.png')
-      $result.text('тоже самое')
-    }
-    else if (chosen[0] === chosen[1]) {
-      $cardsarr.eq(oneId).attr('src', `images/${chosen[0]}.png`)
-      $cardsarr.eq(twoId).attr('src', `images/${chosen[1]}.png`)    
-      $cardsarr.eq(oneId).off('click', flip)
-      $cardsarr.eq(twoId).off('click', flip)
-      won.push(chosen)
-      $current.text('попал, +1 очко')
-    } else {
-      $cardsarr.eq(oneId).attr('src', 'images/blank.png')
-      $cardsarr.eq(twoId).attr('src', 'images/blank.png')
-      $current.text('не попал')
-    }
-    chosen = []
-    chosenId = []
-    $result.text(won.length)
-    if (won.length === cardsarr.length/2) {
-      $current.text('победа')
-    }
-    lock = false
+      const $cardsarr = $('img');
+      const oneId = chosenId[0];
+      const twoId = chosenId[1];
+      
+      attempt++;
+      
+      if (oneId == twoId) {
+          $cardsarr.eq(oneId).attr('src', 'images/blank.png');
+          $cardsarr.eq(twoId).attr('src', 'images/blank.png');
+          $current.text(`попытка ${attempt}: та же карта`);
+      }
+      else if (chosen[0] === chosen[1]) {
+          const firstGoldCard = cardsarr[oneId].isGold;
+          const secondGoldCard = cardsarr[twoId].isGold;
+          
+          won.push(chosen);
+          
+          let points = 0;
+          
+          if (attempt === 1) {
+              points = 10;
+              $current.text('попал, +10 очков');
+          } else if (attempt === 2) {
+              points = 5;
+              $current.text('попал, +5 очков');
+          } else {
+            $current.text('попал, +0 очков');
+          }
+          
+          if ((firstGoldCard && secondGoldCard) && attempt <= 2) {
+              founded = true;
+              points *= 2;
+              $current.text($current.text() + ' золотая пара, очки x2');
+          }
+          
+          let currentScore = parseInt($result.text()) || 0;
+          $result.text(currentScore + points);
+          
+          $cardsarr.eq(oneId).attr('src', `images/${chosen[0]}.png`);
+          $cardsarr.eq(twoId).attr('src', `images/${chosen[1]}.png`);    
+          $cardsarr.eq(oneId).off('click', flip);
+          $cardsarr.eq(twoId).off('click', flip);
+          
+      } else {
+          $cardsarr.eq(oneId).attr('src', 'images/blank.png');
+          $cardsarr.eq(twoId).attr('src', 'images/blank.png');
+          $current.text(`не попал`);
+      }
+      
+      chosen = [];
+      chosenId = [];
+      update();
+      
+      if (won.length === cardsarr.length / 2) {
+          $current.text(`победа! всего попыток: ${attempt}`);
+      }
+      lock = false;
   }
 
   function flip() {
-    if (lock) return '' // блокировка события click
-    $('#btn').text("Завершить")
-    const $this = $(this)
-    let id = $this.attr('data-id')
-    chosen.push(cardsarr[id].name)
-    chosenId.push(id)
-    $this.attr('src', cardsarr[id].img)
-    if (chosen.length === 2) {
-      lock = true
-      setTimeout(check, 500)
-    }
+      if (lock) return '';
+      $('#btn').text("Завершить");
+      const $this = $(this);
+      let id = $this.attr('data-id');
+      if ($this.attr('src') !== 'images/blank.png') return '';
+      chosen.push(cardsarr[id].name);
+      chosenId.push(id);
+      $this.attr('src', cardsarr[id].img);
+      
+      if (chosen.length === 2) {
+          lock = true;
+          setTimeout(check, 500);
+      }
   }
-
-  create()
-})
+  update()
+  create();
+});
